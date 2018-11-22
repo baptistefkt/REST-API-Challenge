04a1d5186d3ac2ebfef31639cb7738ba36d02769
baptistefkt <baptistefirket@gmail.com>
2018-11-22 17:04:31 +0100

    test fixed

--- test/appTest.js
+++ test/appTest.js
@@ -1,18 +1,19 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const index = require('../index');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const index = require("../index");
 const expect = chai.expect;
 
 chai.use(chaiHttp);
-describe('test', () => {
-    it('res.status equals 200 and res.body.success equals true', (done) => {
-        chai.request(index)
-            .post('/api/v1/quote/car-insurance')
-            .send({ "car_value": "2000", "driver_birthdate": "10/06/1990" })
-            .end(function (req, res) {
-                expect(res).to.have.status(200);
-                expect(res.body.success).to.equal(true);
-            })
-        done();
-    });
+describe("test", () => {
+  it("res.status equals 200 and res.body.success equals true", done => {
+    chai
+      .request(index)
+      .post("/v1/quote/car-insurance")
+      .send({ car_value: "2000", driver_birthdate: "10/06/1990" })
+      .end(function(req, res) {
+        expect(res).to.have.status(200);
+        expect(res.body.success).to.equal(true);
+      });
+    done();
+  });
 });
