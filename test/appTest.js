f07fc19943b3181d1ee1e30e8ef63e9bb2c1991d
baptistefkt <baptistefirket@gmail.com>
2018-11-23 13:57:31 +0100

    full test implemented

--- test/appTest.js
+++ test/appTest.js
@@ -4,8 +4,109 @@ const index = require("../index");
 const expect = chai.expect;
 
 chai.use(chaiHttp);
-describe("test", () => {
-  it("res.status equals 200 and res.body.success equals true", done => {
+
+describe("wrong route", () => {
+  it("res.status equals 404", done => {
+    chai
+      .request(index)
+      .post("/wrong_route")
+      .send({ car_value: "2000", driver_birthdate: "10/06/1990" })
+      .end(function(req, res) {
+        expect(res).to.have.status(404);
+      });
+    done();
+  });
+});
+
+describe("params missing", () => {
+  it("res.status equals 400 and res.body.success equals false", done => {
+    chai
+      .request(index)
+      .post("/v1/quote/car-insurance")
+      .send()
+      .end(function(req, res) {
+        expect(res).to.have.status(400);
+        expect(res.body.success).to.equal(false);
+      });
+    done();
+  });
+});
+
+describe("one of params missing", () => {
+  it("res.status equals 400 and res.body.success equals false", done => {
+    chai
+      .request(index)
+      .post("/v1/quote/car-insurance")
+      .send({ driver_birthdate: "10/06/1990" })
+      .end(function(req, res) {
+        expect(res).to.have.status(400);
+        expect(res.body.success).to.equal(false);
+      });
+    done();
+  });
+});
+
+describe("params wrong value", () => {
+  it("res.status equals 400 and res.body.success equals false", done => {
+    chai
+      .request(index)
+      .post("/v1/quote/car-insurance")
+      .send({ car_value: "20A0", driver_birthdate: "10/06/1990" })
+      .end(function(req, res) {
+        expect(res).to.have.status(400);
+        expect(res.body.success).to.equal(false);
+      });
+    done();
+  });
+});
+
+describe("params wrong value", () => {
+  it("res.status equals 400 and res.body.success equals false", done => {
+    chai
+      .request(index)
+      .post("/v1/quote/car-insurance")
+      .send({ car_value: "2000", driver_birthdate: "10//06/1990" })
+      .end(function(req, res) {
+        expect(res).to.have.status(400);
+        expect(res.body.success).to.equal(false);
+      });
+    done();
+  });
+});
+
+describe("case under 18", () => {
+  it("res.status = 200, res.body.success = true and res.body.data.eligible = false", done => {
+    chai
+      .request(index)
+      .post("/v1/quote/car-insurance")
+      .send({ car_value: "2000", driver_birthdate: "10/06/2005" })
+      .end(function(req, res) {
+        expect(res).to.have.status(200);
+        expect(res.body.success).to.equal(true);
+        expect(res.body.data.eligible).to.equal(false);
+      });
+    done();
+  });
+});
+
+describe("case under between 18 and 26", () => {
+  it("res.status = 200, res.body.success = true, res.body.data.eligible = true and res.body.data.premiums.civil_liability = 1000 ", done => {
+    chai
+      .request(index)
+      .post("/v1/quote/car-insurance")
+      .send({ car_value: "2000", driver_birthdate: "10/06/1997" })
+      .end(function(req, res) {
+        expect(res).to.have.status(200);
+        expect(res.body.success).to.equal(true);
+        expect(res.body.data.eligible).to.equal(true);
+        expect(res.body.data.premiums.civil_liability).to.equal(1000);
+      });
+    done();
+  });
+});
+
+describe("case 26+", () => {
+  it("res.status = 200, res.body.success = true, res.body.data.eligible = true and res.body.data.premiums.civil_liability = 500", done => {
     chai
       .request(index)
       .post("/v1/quote/car-insurance")
@@ -13,6 +114,8 @@ describe("test", () => {
       .end(function(req, res) {
         expect(res).to.have.status(200);
         expect(res.body.success).to.equal(true);
+        expect(res.body.data.eligible).to.equal(true);
+        expect(res.body.data.premiums.civil_liability).to.equal(500);
       });
     done();
   });
